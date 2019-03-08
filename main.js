88f46ab84e8267c2057c99352be1cd4775463d56
h22121 <h22121@lab.cpcc.edu>
2019-03-07 20:56:16 -0500

    3/7/19 classwork

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 $(document).ready(function (){
     console.log("You are reading this from the console");
+
+    $("h1").css("color", "red");
+
+    paper.install(window);
+    paper.setup(document.getElementById("mainCanvas"));
+
+    var c = Shape.Circle(200,200, 50);
+
+    c.fillColor = "green";
+
+    paper.view.draw();
+
+
 });
